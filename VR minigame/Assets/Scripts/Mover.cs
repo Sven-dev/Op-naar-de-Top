a979fb1e45a19b2cfd3faa048ef547409e9cf433
Unknown <svenjanssen12321@gmail.com>
2019-05-06 16:33:50 +0200

    Trying to get ball to behave
    w/ Estée

---
+++ VR minigame/Assets/Scripts/Mover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public Transform StableObject;
+    public Transform ARObject;
+    public bool Tapped;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Tapped)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+	}
+
+    void OnMouseDown()
+    {
+        Tapped = true;
+        transform.parent = ARObject;
+    }
+}

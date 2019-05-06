378d68ae7c4a32b6e4cf6341c8978d86396704c1
Unknown <svenjanssen12321@gmail.com>
2019-05-06 13:12:26 +0200

    Got AR working
    w/ Estée

---
+++ VR minigame/Assets/Scripts/ShootButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootButton : MonoBehaviour {
+
+    public bool Hit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Hit)
+        {
+            Hit = false;
+            Shoot();
+        }
+	}
+
+    //Launches a ball
+    private void Shoot()
+    {
+
+    }
+}

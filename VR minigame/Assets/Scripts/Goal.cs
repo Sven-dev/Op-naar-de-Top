06abfab42540cda8ed28573f8f6d6e3694189f90
Estée <e.verhorevoort@gmail.com>
2019-05-07 11:43:40 +0200

    Score function

--- VR minigame/Assets/Scripts/Goal.cs
+++ VR minigame/Assets/Scripts/Goal.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Goal : MonoBehaviour {
-
+    public Text ScoreText;
+    public int Score;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Ball")
+        {
+            Score = +1;
+            ScoreText.text = Score.ToString();
+
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        
+
+    }
 }

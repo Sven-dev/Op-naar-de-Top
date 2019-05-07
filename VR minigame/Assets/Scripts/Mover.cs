60132b2db04fd813b290d3f18ba90c1c3ed58cfa
Unknown <svenjanssen12321@gmail.com>
2019-05-07 16:01:08 +0200

    UI cleanup + physics tweak

--- VR minigame/Assets/Scripts/Mover.cs
+++ VR minigame/Assets/Scripts/Mover.cs
@@ -8,16 +8,18 @@ public class Mover : MonoBehaviour
     private Rigidbody rb;
 
 	// Use this for initialization
-	void Start ()
+	void Start()
     {
-        GetComponent<Rigidbody>();
         rb = GetComponent<Rigidbody>();
+        print("RB:" + rb);
 	}
 
     private void OnMouseDown()
     {
         transform.parent = ARObject;
-        rb.AddForce(Vector3.forward * 35);
+        rb.AddForce(Vector3.forward * 150);
+        rb.useGravity = true;
+        tag = "Ball";
     }
 
     //Sets the variables when the object is spawned
